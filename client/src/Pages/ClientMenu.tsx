1545a8cee4c19c8fc740df355b4e40c0bf6b86d3
Brando Lugo <brando.lugo@gmail.com>
2022-01-11 11:37:54 -0600

    Added a client panel

--- client/src/Pages/ClientMenu.tsx
+++ client/src/Pages/ClientMenu.tsx
@@ -2,7 +2,27 @@ import React from "react";
 
 class ClientMenu extends React.Component {
   render() {
-    return <>Client Menu</>;
+    return (
+      <div>
+        <div className="grid grid-cols-3 gap-4">
+          <div>
+            <button>Test</button>
+          </div>
+          <div>
+            <button>Test</button>
+          </div>
+          <div>
+            <button>Test</button>
+          </div>
+          <div>
+            <button>Test</button>
+          </div>
+          <div>
+            <button>Test</button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
