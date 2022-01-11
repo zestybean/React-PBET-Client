22d8d1ad4ed8a74d3b4bf9074780c4413de1884d
Brando Lugo <brando.lugo@gmail.com>
2022-01-11 10:53:40 -0600

    Added some styling with tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import ClientMenu from "./Pages/ClientMenu";
+
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-bold underline">Hello world!</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<>Home</>} />
+          <Route path="/clientmenu" element={<ClientMenu />} />
+          <Route path="/adminlogin" element={<>Report Login</>} />
+          <Route path="/reports" element={<>Reports</>} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ client/src/Pages/ClientMenu.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ClientMenu extends React.Component {
+  render() {
+    return <>Client Menu</>;
+  }
+}
+
+export default ClientMenu;
