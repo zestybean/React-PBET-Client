71bc1911dc8067852d9be738fe05262bda91a730
zestybean <brando.lugo@gmail.com>
2022-01-12 22:05:41 -0600

    Addded formik and a basic login form

--- client/src/Components/SubmitButton.tsx
+++ client/src/Components/SubmitButton.tsx
@@ -5,6 +5,7 @@ function SubmitButton(props: any) {
     <>
       <button
         type="submit"
+        disabled={props.disabled}
         className="bg-emerald-700 hover:bg-emerald-600 text-white font-bold py-2 px-8 rounded"
       >
         {props.title}
--- client/src/Pages/ClientLogin.tsx
+++ client/src/Pages/ClientLogin.tsx
@@ -1,39 +1,52 @@
 import React from "react";
 import SubmitButton from "../Components/SubmitButton";
-
-import MenuButton from "../Components/SubmitButton";
+import { Formik, Field, Form } from "formik";
 
 function ClientLogin(props: any) {
   return (
     <>
-      <div className="h-screen flex flex-col justify-start items-center mt-64">
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-green-700 text-bold text-2xl mb-6">
-            PBET Client Login
-          </h1>
-
-          <form className="flex flex-col justify-center items-center">
-            <input
-              aria-label="Enter your email address"
-              type="text"
-              placeholder="PBET ID"
-              className="text-sm text-gray-base w-full 
+      <Formik
+        initialValues={{ pbetID: "", pbetPass: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          setSubmitting(true);
+          //Async call
+          alert(JSON.stringify(values, null, 2));
+          console.log(values);
+          setSubmitting(false);
+        }}
+      >
+        {({ values, isSubmitting }) => (
+          <div className="p-10">
+            <div className="flex flex-col justify-start items-center mt-32">
+              <div className="flex flex-col justify-center items-center">
+                <h1 className="text-green-700 font-bold text-4xl mb-">
+                  Pace Board Entry Tool
+                </h1>
+                <h1 className="text-green-600 text-2xl mb-8">Client Login</h1>
+                <Form className="flex flex-col justify-center items-center">
+                  <Field
+                    name="pbetID"
+                    type="input"
+                    placeholder="PBET ID"
+                    className="text-sm text-gray-base w-full 
                       mr-3 py-5 px-4 h-2 border 
                       border-gray-200 rounded mb-2"
-            />
-            <input
-              aria-label="Enter your password"
-              type="password"
-              placeholder="PBET Password"
-              className="text-sm text-gray-base w-full mr-3 
-                      py-5 px-4 h-2 border border-gray-200 
-                      rounded mb-6"
-            />
-
-            <SubmitButton title="Login" />
-          </form>
-        </div>
-      </div>
+                  />
+                  <Field
+                    name="pbetPass"
+                    type="password"
+                    placeholder="PBET Password"
+                    className="text-sm text-gray-base w-full 
+                      mr-3 py-5 px-4 h-2 border 
+                      border-gray-200 rounded mb-2"
+                  />
+                  <SubmitButton disabled={isSubmitting} title="Login" />
+                </Form>
+              </div>
+            </div>
+          </div>
+        )}
+      </Formik>
     </>
   );
 }
